ecb6cb53595b465c6c6765e69d27e45e8ac0784b
Robert Kaye <rob@partyrobotics.com>
2013-06-20 10:13:32 +0200

    A pile of code that doesn't run right. But code got written!

--- firmware/ledtree.c
+++ firmware/ledtree.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <stdint.h>
+#include <string.h>
 #include <stdlib.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
@@ -11,6 +12,12 @@
 #define cbi(a, b) ((a) &= ~(1 << (b)))    //clears bit B in variable A
 #define tbi(a, b) ((a) ^= 1 << (b))       //toggles bit B in variable A
 
+#define PARENT PD7
+#define CHILD0 PD4
+#define CHILD1 PD2
+
+void process_command(char *cmd);
+
 void delay_ms(uint16_t d)
 {
     uint16_t i;
@@ -21,25 +28,25 @@ void delay_ms(uint16_t d)
 
 void pwm_setup(void)
 {
-	/* Set to Fast PWM */
-	TCCR0A |= _BV(WGM01) | _BV(WGM00);
-	TCCR2A |= _BV(WGM21) | _BV(WGM20);
+    /* Set to Fast PWM */
+    TCCR0A |= _BV(WGM01) | _BV(WGM00);
+    TCCR2A |= _BV(WGM21) | _BV(WGM20);
 
-	// Set the compare output mode
-	TCCR0A |= _BV(COM0A1);
-	TCCR0A |= _BV(COM0B1);
-	TCCR2A |= _BV(COM2B1);
+    // Set the compare output mode
+    TCCR0A |= _BV(COM0A1);
+    TCCR0A |= _BV(COM0B1);
+    TCCR2A |= _BV(COM2B1);
 
-	// Reset timers and comparators
-	OCR0A = 0;
-	OCR0B = 0;
-	OCR2B = 0;
-	TCNT0 = 0;
-	TCNT2 = 0;
+    // Reset timers and comparators
+    OCR0A = 0;
+    OCR0B = 0;
+    OCR2B = 0;
+    TCNT0 = 0;
+    TCNT2 = 0;
 
     // Set the clock source
-	TCCR0B |= _BV(CS00);
-	TCCR2B |= _BV(CS20);
+    TCCR0B |= _BV(CS00);
+    TCCR2B |= _BV(CS20);
 
     // Set PWM pins as outputs
     DDRD |= (1<<PD6)|(1<<PD5)|(1<<PD3);
@@ -56,23 +63,14 @@ void flash_led(void)
 {
     uint8_t i;
 
-    for(i = 0; i < 10; i++)
+    for(i = 0; i < 3; i++)
     {
-        set_led_color(255, 0, 0);
-        _delay_ms(100);
-        set_led_color(0, 0, 0);
-        _delay_ms(100);
-
-        set_led_color(0, 255, 0);
-        _delay_ms(100);
-        set_led_color(0, 0, 0);
-        _delay_ms(100);
-
         set_led_color(0, 0, 255);
-        _delay_ms(100);
-        set_led_color(0, 0, 0);
-        _delay_ms(100);
+        _delay_ms(200);
+        set_led_color(255, 0, 0);
+        _delay_ms(200);
     }
+    set_led_color(0, 0, 0);
 }
 
 int fade_test(void)
@@ -118,23 +116,172 @@ void fade(uint8_t r1, uint8_t g1, uint8_t b1,
     }
 }
 
+#define BIT_DELAY 100
+
+uint8_t receive_byte(uint8_t port)
+{
+    uint8_t i, ch = 0;
+
+    // Wait for the line to become quiet
+    for(;;)
+        if (!(PIND & (1 << port)))
+            break;
+
+    // wait for the start bit
+    for(;;)
+        if (PIND & (1 << port))
+            break;
+
+    for(i = 0; i < 8; i++)
+    {
+        _delay_us(BIT_DELAY);
+
+        if (PIND & (1 << port))
+            ch |= (1 << i);
+    }    
+
+    // Ride out the stop bit
+    _delay_us(BIT_DELAY);
+
+    return ch;
+}
+
+uint8_t send_byte(uint8_t port, uint8_t ch)
+{
+    uint8_t i;
+
+    // send the start bit
+    sbi(PORTD, port);
+    _delay_us(BIT_DELAY);
+
+    for(i = 0; i < 8; i++)
+    {
+        if (ch & (1 << i))
+            sbi(PORTD, port);
+        else
+            cbi(PORTD, port);
+        _delay_us(BIT_DELAY);
+    }
+
+    // Send the stop bit
+    cbi(PORTD, port);
+    _delay_us(BIT_DELAY);
+}
+
+uint8_t send_command(uint8_t port, char *cmd)
+{
+    uint8_t num = 0, ack;
+    char *ptr;
+
+    for(;;)
+    {
+        // Prepare to send over the given pin
+        DDRD |= (1<<port);
+
+        for(ptr = cmd; *ptr; ptr++)
+            send_byte(port, *ptr);
+
+        // Prepare to receive over the given pin
+        DDRD &= ~(1<<port);
+
+        ack = receive_byte(port);
+        if (ack == 0)
+            break;
+    }
+
+    ptr = cmd;
+    for(;;)
+    {
+        *ptr = receive_byte(port);
+        if (*ptr == '\n')
+        {
+            *ptr = 0;
+            return num;
+        }
+        ptr++;
+        num++;
+    }
+
+    // Switch back to transmit
+    DDRD |= (1<<port);
+
+    // Send ack
+    send_byte(port, 0);
+}
+
+uint8_t receive_command(uint8_t port, char *cmd)
+{
+    uint8_t num = 0, ack;
+    char *ptr = cmd;
+
+    // Prepare to receive over the given pin
+    DDRD &= ~(1<<port);
+
+    for(;;)
+    {
+        *ptr = receive_byte(port);
+        if (*ptr == '\n')
+        {
+            *ptr = 0;
+            return num;
+        }
+        ptr++;
+        num++;
+    }
+
+    // Switch to transmit
+    DDRD |= (1<<port);
+    send_byte(port, 0);
+
+    process_command(cmd);
+
+    for(;;)
+    {
+        // Prepare to send over the given pin
+        DDRD |= (1<<port);
+
+        for(ptr = cmd; *ptr; ptr++)
+            send_byte(port, *ptr);
+
+        // Prepare to receive over the given pin
+        DDRD &= ~(1<<port);
+
+        ack = receive_byte(port);
+        if (ack == 0)
+            break;
+    }
+
+    // Back to receive
+    DDRD &= ~(1<<port);
+}
+
+void process_command(char *cmd)
+{
+    if (strcmp(cmd, "boo!") == 0)
+        set_led_color(0, 0, 255);
+    else
+        set_led_color(255, 0, 0);
+}
+
 int main(void)
 {
-    uint8_t i, r1, g1, b1, r2, b2, g2;
+    char cmd[32];
 
     pwm_setup();
     flash_led();
 
-    r1 = g1 = b1 = 0;
-	while (1)
-    {
-        r2 = random() % 255;
-        g2 = random() % 255;
-        b2 = random() % 255;
-        fade(r1, g1, b1, r2, g2, b2, 100, 10);
-        r1 = r2;
-        g1 = g2;
-        b1 = b2;
-	}
-	return 0;
+#if 1
+    strcpy(cmd, "boo!");
+    send_command(CHILD0, cmd);
+
+    if (strcmp(cmd, "boo!") == 0)
+        set_led_color(0, 0, 255);
+    else
+        set_led_color(255, 0, 0);
+#else
+    for(;;)
+        receive_command(PARENT, cmd);
+#endif
+
+    return 0;
 }
